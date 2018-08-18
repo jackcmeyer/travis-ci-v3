7ce6a1c63f33269f28830e8287f3682b377f428b
Jack Meyer <jackcmeyer@gmail.com>
2018-08-18 18:06:10 -0500

    removes files that should have been ignored

--- src/api/UserAPI.d.ts
+++
@@ -1,5 +0,0 @@
-export default class UserAPI {
-    constructor();
-    getUser(token: string): Promise<User>;
-    getUserById(userId: number, token: string): Promise<User>;
-}
--- src/util/HttpUtil.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function get(uri: string, token: string): Promise<any>;
