d5458fd195a24f0aa02a6df19aa40937ca3ec29b
Jack Meyer <jackcmeyer@gmail.com>
2018-08-18 18:01:18 -0500

    adds error handling to the http call

--- src/util/HttpUtil.ts
+++ src/util/HttpUtil.ts
@@ -4,13 +4,18 @@ const travisUrl = "https://api.travis-ci.org";
 
 
 export async function get(uri: string, token: string) {
-    const response = await buildGetRequest(uri, token);
-    return JSON.parse(response.body);
+    try {
+        const response = await buildGetRequest(uri, token);
+        return JSON.parse(response.body);
+    } catch (error) {
+        throw new Error(error.response.body.error_message);
+    }
 }
 
 function buildGetRequest(uri: string, token: string) {
-    const travis = got.extend({
+    const api = got.extend({
         baseUrl: travisUrl,
+
         headers: {
             'Authorization': 'token ' + token,
             'Travis-API-Version': 3,
@@ -18,5 +23,5 @@ function buildGetRequest(uri: string, token: string) {
         }
     });
 
-    return travis.get(uri);
+    return api.get(uri);
 }
